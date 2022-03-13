f52c901e6e40f68508665ead726fabd9a9ec0d13
yuanting <1967368231@qq.com>
2022-03-13 16:09:31 +0800

    fix bug for can't repeat request

--- pkg/client/kubernetes.go
+++ pkg/client/kubernetes.go
@@ -9,36 +9,46 @@ import (
 	"k8s.io/client-go/util/homedir"
 	"k8s.io/klog"
 	"path/filepath"
+	"sync"
 )
 
+var onceClient sync.Once
+var onceConfig sync.Once
+var Kubeconfig *rest.Config
+var KubeClientSet *kubernetes.Clientset
+
 func GetK8sClientSet() (*kubernetes.Clientset, error) {
-	config, err := GetRestConfig()
-	if err != nil {
-		return nil, err
-	}
-	clientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		klog.Fatal(err)
-		return nil, err
-	}
-	return clientSet, nil
+	onceClient.Do(func() {
+		config, err := GetRestConfig()
+		if err != nil {
+			return
+		}
+		KubeClientSet, err = kubernetes.NewForConfig(config)
+		if err != nil {
+			klog.Fatal(err)
+			return
+		}
+	})
+	return KubeClientSet, nil
 }
 
 func GetRestConfig() (config *rest.Config, err error) {
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "")
-	} else {
-		klog.Fatal("read config error, config is empty")
-		err = errors.New("read config error, config is empty")
+	onceConfig.Do(func() {
+		var kubeconfig *string
+		if home := homedir.HomeDir(); home != "" {
+			kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "")
+		} else {
+			klog.Fatal("read config error, config is empty")
+			err = errors.New("read config error, config is empty")
+			return
+		}
+		flag.Parse()
+		Kubeconfig, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
+		if err != nil {
+			klog.Fatal(err)
+			return
+		}
 		return
-	}
-	flag.Parse()
-	config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig)
-	if err != nil {
-		klog.Fatal(err)
-		return
-	}
-	return
-
+	})
+	return Kubeconfig, nil
 }
